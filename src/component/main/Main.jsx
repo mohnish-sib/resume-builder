2313c485b666d6e2aa296fa658d2495c5dc4a52e
mohnish-sib <mohnish.lokhande@sendinblue.com>
2023-03-22 10:27:19 +0530

    basic box

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./Main.module.css";
 
 export default function Main() {
-  return <>eheadhb</>;
+  return <div className={styles.mainContainer}>hehhehe</div>;
 }
