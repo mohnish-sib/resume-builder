f3542b12c93509f5f11882a76a00f1cc83179a93
mohnish-sib <mohnish.lokhande@sendinblue.com>
2023-04-12 17:17:28 +0530

    download pdf

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { createRef } from "react";
 import { useSelector } from "react-redux";
 import ExperienceBlock from "../ExperienceBlock";
 import Intro from "../Intro";
 import ResumeEditor from "../ResumeEditor";
 import styles from "./Main.module.css";
+import Pdf from "react-to-pdf";
 
 export default function Main() {
   const { educations = [] } = useSelector((state) => state.Education);
 
-  console.log("###", educations);
+  const ref = createRef();
+  // console.log("###", educations);
   return (
     <div className={styles.mainContainer}>
       <ResumeEditor />
-      <div style={{ width: "50vw" }}>
+      <div className={styles.resume} ref={ref}>
         <div className={styles.resumeContainer}>
+          <Pdf
+            targetRef={ref}
+            filename="code-example.pdf"
+            scale={1}
+            x={10}
+            y={0}
+          >
+            {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+          </Pdf>
           <Intro />
           <ExperienceBlock heading="Experiance" content={educations} />
         </div>
